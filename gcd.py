013831b9488fa21891f60dcd129e5c548cf049d4
Fazrul Rahman <fzrlrhmn@gmail.com>
2018-08-04 14:53:08 +0530

    Day 4

---
+++ gcd.py
@@ -0,0 +1,17 @@
+m = 20
+n = 4
+
+sol = 0
+
+for x in range(1,max(m,n)):
+	if m % x == 0 and n % x == 0:
+		sol = x
+
+print sol
+
+def gcd(a,b):
+	if b == 0:
+		return a
+	else:
+		return gcd(b, a % b)
+print gcd(5,6)
