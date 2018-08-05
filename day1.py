8b50b1f84ca7e234e7c28ca8db9a598b6ffd549d
Fazrul Rahman <fzrlrhmn@gmail.com>
2018-08-05 15:49:33 +0530

    Day 1
    This problem was recently asked by Google.
    
    Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
    
    For example, given [10, 15, 3, 7] and k of 17, return true since 10 + 7 is 17.
    
    Bonus: Can you do this in one pass?

--- day1.py
+++ day1.py
@@ -1,1 +1,21 @@
-n = []
+n = [12, 5, 6, 2]
+k = 11
+
+n.sort()
+start = 0
+end = len(n) - 1
+flag = 0
+
+while start != end:
+	if n[start] + n[end] == k:
+		flag = 1
+		start = end
+	elif n[start] + n[end] < k:
+		start = start + 1
+	elif n[start] + n[end] > k:
+		end = end - 1
+
+if flag == 1:
+	print "True"
+else:
+	print "False"
