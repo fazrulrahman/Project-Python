d8c4f28d94ee00a2b045640365b1d5aed45f5359
Fazrul Rahman <fzrlrhmn@gmail.com>
2018-08-07 21:53:32 +0530

    day 8

---
+++ linearregression.py
@@ -0,0 +1,27 @@
+import sklearn.linear_model as lm
+import csv
+
+import warnings
+warnings.filterwarnings(action="ignore", module="sklearn", message="internal gelsd")
+
+inputX = []
+outputY = []
+textInput = []
+
+with open("train.csv",'rb') as csvtrain:
+	reader = csv.reader(csvtrain)
+	reader.next()
+	for row in reader:
+		inputX.append([row[0]])
+		outputY.append(row[1])
+
+lr = lm.LinearRegression()
+lr.fit(inputX,outputY)
+
+with open("test.csv",'rb') as csvtest:
+	reader = csv.reader(csvtest)
+	reader.next()
+	for row in reader:
+		textInput.append([float(row[0])])
+
+print lr.predict(textInput)
