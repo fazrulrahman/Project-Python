ad9a7c31d7fa7d6e4cb341e3605278906bab6003
Fazrul Rahman <fzrlrhmn@gmail.com>
2018-08-08 21:22:54 +0530

    candles

--- candles.py
+++ candles.py
@@ -1,2 +1,18 @@
-n = 5
+n = input()
 
+# candles = [3,2,1,3]
+candles = []
+for i in range(0,n):
+	candles.append(input())
+
+count = 0
+tallCandle = 0
+
+for i in range(0,n):
+
+	if(candles[i] == tallCandle):
+		count = count + 1
+	elif(candles[i] > tallCandle):
+		tallCandle = candles[i]
+		count = 1
+print count
